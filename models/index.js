9f606f1f5ac39ad99aeb69f6ad9e9a8221573894
Brian Wyatt <brianlwyatt@gmail.com>
2023-02-12 08:37:02 -0400

    changed main.handlebars

--- models/index.js
+++ models/index.js
@@ -1,4 +1,11 @@
 const User = require('./User');
 const Posts = require('./Posts');
 
+User.hasMany(Posts, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Posts.belongsTo(User);
+
 module.exports = { User, Posts };
